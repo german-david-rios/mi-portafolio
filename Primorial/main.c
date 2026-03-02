03652f4cb3725b271bb67a712cea267858f96c7b
Germán David Ríos <germanrios1435@gmail.com>
2026-03-02 10:06:02 -0300

    Update main.c
    Se arreglaron cuestiones de números

--- Primorial/main.c
+++ Primorial/main.c
@@ -4,7 +4,8 @@
 int isPrime(int n){
     if(n<=1) return 0;
     if(n%2==0) return (n==2);
-    for(int div = 3, step = 2, roof = (int) sqrt(n);div<roof;div+=step, step^=6){
+    if(n%3==0) return (n==3);
+    for(int div = 5, step = 2, roof = (int) sqrt(n);div<roof;div+=step, step^=6){
         if(n%div == 0) return 0;
     }
     else return 1;
@@ -14,8 +15,8 @@ int main(){
     unsigned int primorial = 0; int n = 0;
     puts("Escriba el primorial que quiera calcular:")
     scanf("%d",&n);
-    if(n>1) primorial=2; 
-    for(int i = 3, step=2; i <= n; i+=step, step^=6){
+    if(n>1) primorial = 1 * ((n%2==0)?2:1) * ((n%3==0)?3:1);
+    for(int i = 5, step=2; i <= n; i+=step, step^=6){
          if(isPrime(i))
              primorial *= i;
     }
