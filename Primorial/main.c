4993ed4fac39b8c0e69d7d723a235a238996980b
Germán David Ríos <germanrios1435@gmail.com>
2026-05-08 23:13:34 -0300

    Update main.c
    logical error fixed

--- Primorial/main.c
+++ Primorial/main.c
@@ -5,7 +5,7 @@ int isPrime(int n){
     if(n<=1) return 0;
     if(n%2==0) return (n==2);
     if(n%3==0) return (n==3);
-    for(int div = 5, step = 2, roof = (int) sqrt(n);div<roof;div+=step, step^=6){
+    for(int div = 5, step = 2, roof = (int) sqrt(n);div<=roof;div+=step, step^=6){
         if(n%div == 0) return 0;
     }
     else return 1;
