fc0ce8f6818bd0963c1be024a36b8ba17ae19b2c
Germán David Ríos <germanrios1435@gmail.com>
2026-05-08 23:33:58 -0300

    Update main.c
    add limits check
    now is pretier

--- Primorial/main.c
+++ Primorial/main.c
@@ -12,15 +12,21 @@ int isPrime(int n){
 }
 
 int main(){
-    unsigned int primorial = 0; int n = 0;
-        puts("Escriba el primorial que quiera calcular:");
-        scanf("%d",&n);
+    unsigned long long primorial = 0; int n = 0;
+    puts("Escriba el primorial que quiera calcular [max 52]:");
+    scanf("%d",&n);
+
+    if(n < 53){
         if(n>1) primorial = 1 * ((n>=2)?2:1) * ((n>=3)?3:1);
         for(int i = 5, step=2; i <= n; i+=step, step^=6){
-             if(isPrime(i))
-                 primorial *= i;
+            if(isPrime(i))
+                primorial *= i;
         }
-        printf("%d# = %u\n",n,primorial);
+        printf("%d# = %llu\n",n,primorial);
+    } else {
+        puts("no es posible calcular ese primorial debido a que sobrepasa el limite");
+    }
+
 
     return 0;
 }
