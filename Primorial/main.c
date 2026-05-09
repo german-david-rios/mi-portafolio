dd6377215f181f7331dc5607d6f50c9ee6d8d3e5
Germán David Ríos <germanrios1435@gmail.com>
2026-05-08 23:22:33 -0300

    Update main.c
    sintax errors fixed
    logical errors fixed
    
    now it works 😃

--- Primorial/main.c
+++ Primorial/main.c
@@ -8,18 +8,19 @@ int isPrime(int n){
     for(int div = 5, step = 2, roof = (int) sqrt(n);div<=roof;div+=step, step^=6){
         if(n%div == 0) return 0;
     }
-    else return 1;
+    return 1;
 }
 
 int main(){
     unsigned int primorial = 0; int n = 0;
-    puts("Escriba el primorial que quiera calcular:")
-    scanf("%d",&n);
-    if(n>1) primorial = 1 * ((n%2==0)?2:1) * ((n%3==0)?3:1);
-    for(int i = 5, step=2; i <= n; i+=step, step^=6){
-         if(isPrime(i))
-             primorial *= i;
-    }
-    printf("%d# = %u\n",n,primorial);
+        puts("Escriba el primorial que quiera calcular:");
+        scanf("%d",&n);
+        if(n>1) primorial = 1 * ((n>=2)?2:1) * ((n>=3)?3:1);
+        for(int i = 5, step=2; i <= n; i+=step, step^=6){
+             if(isPrime(i))
+                 primorial *= i;
+        }
+        printf("%d# = %u\n",n,primorial);
+
     return 0;
 }
